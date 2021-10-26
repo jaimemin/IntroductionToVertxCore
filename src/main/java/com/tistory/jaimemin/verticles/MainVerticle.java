0b61c49e72b65cb8fe14761b8cef7558fc7d9056
jaimemin <jaimemin@gmail.com>
2021-10-26 17:44:37 +0900

    Vert.x Verticles

---
+++ src/main/java/com/tistory/jaimemin/verticles/MainVerticle.java
@@ -0,0 +1,24 @@
+package com.tistory.jaimemin.verticles;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+
+public class MainVerticle extends AbstractVerticle {
+
+  // parents are always started first
+  public static void main(String[] args) {
+    final Vertx vertx = Vertx.vertx();
+    vertx.deployVerticle(new MainVerticle());
+  }
+
+  @Override
+  public void start(Promise<Void> startPromise) throws Exception {
+    System.out.println("Start " + getClass().getName());
+
+    vertx.deployVerticle(new VerticleA());
+    vertx.deployVerticle(new VerticleB());
+    startPromise.complete();
+  }
+}
+
---
+++ src/main/java/com/tistory/jaimemin/verticles/VerticleB.java
@@ -0,0 +1,14 @@
+package com.tistory.jaimemin.verticles;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+
+public class VerticleB extends AbstractVerticle {
+
+  @Override
+  public void start(Promise<Void> startPromise) throws Exception {
+    System.out.println("Start " + getClass().getName());
+
+    startPromise.complete();
+  }
+}
